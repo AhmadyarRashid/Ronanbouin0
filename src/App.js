0df3acc3ace97ecf698c7e80fd1da1fbfbeca738
Ahmad yar meo <ahmadyarmeo@gmail.com>
2019-08-23 00:39:38 +0500

    first update to ronan

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+import { Tabs } from 'antd';
+import MainComponent from './components/MainComponent';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { TabPane } = Tabs;
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  callback = (key) => {
+    console.log(key);
+  };
+
+  render() {
+    return(
+        <div className="App">
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            <TabPane tab="Tab 1" key="1">
+              <MainComponent/>
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>,
+        </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../containers/Header';
+import MainSection from '../containers/MainSection';
+
+
+const MainComponent = () => (
+    <div>
+        <Header />
+        <MainSection />
+    </div>
+);
+
+export default MainComponent;
